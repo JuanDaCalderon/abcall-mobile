f6edb24bb8dff32d357890e74e43c4f0e1261876
juan david calderon jimenez <j.calderonj@uniandes.edu.co>
2024-10-11 22:38:05 -0500

    feat: integrate with backend

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {TranslateService} from '@ngx-translate/core';
-import {take} from 'rxjs';
+import {catchError, take} from 'rxjs';
 import {Router} from '@angular/router';
 import {ToastController} from '@ionic/angular';
 import {DEFAULT_LANG} from 'src/constants';
@@ -37,20 +37,38 @@ export class LoginPage {
     this.isLoading = true;
     this.authService
       .login(this.loginForm.get('email')?.value, this.loginForm.get('password')?.value)
-      .pipe(take(1))
-      .subscribe(async (_value) => {
-        const toast = await this.toastController.create({
-          message: this.translateService.instant('abc.toast.success.message'),
-          duration: 5000,
-          cssClass: 'fs-normal',
-          color: 'primary',
-          icon: 'checkmark-done-outline',
-          position: 'bottom',
-          swipeGesture: 'vertical'
-        });
-        toast.present();
-        this.isLoading = false;
-        this.router.navigateByUrl('tabs');
+      .pipe(
+        take(1),
+        catchError(async (error) => {
+          const errorMsg: string = `${this.translateService.instant('abc.toast.incorrect.message')} ${error && error.error?.message ? (error.error?.message as string).toLowerCase() : ''}`;
+          const toast = await this.toastController.create({
+            message: errorMsg,
+            duration: 5000,
+            cssClass: 'fs-normal',
+            color: 'danger',
+            icon: 'alert-circle-outline',
+            position: 'bottom',
+            swipeGesture: 'vertical'
+          });
+          toast.present();
+          this.isLoading = false;
+        })
+      )
+      .subscribe(async (value) => {
+        if (!!value) {
+          const toast = await this.toastController.create({
+            message: this.translateService.instant('abc.toast.success.message'),
+            duration: 5000,
+            cssClass: 'fs-normal',
+            color: 'primary',
+            icon: 'checkmark-done-outline',
+            position: 'bottom',
+            swipeGesture: 'vertical'
+          });
+          toast.present();
+          this.isLoading = false;
+          this.router.navigateByUrl('tabs');
+        }
       });
   }
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,11 +1,14 @@
 import {Injectable} from '@angular/core';
-import {delay, Observable, of} from 'rxjs';
-
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor() {}
+  /** API url */
+  private apiUrl = environment.apiUrl;
+  constructor(private http: HttpClient) {}
 
   /**
    * Login the user to the backend
@@ -13,7 +16,7 @@ export class AuthService {
    * @param {string} password Password of the user
    * @returns {Observable<string>}
    */
-  public login(email: string, password: string): Observable<string> {
-    return of(email + '----' + password).pipe(delay(2000));
+  public login(email: string, password: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/usuario/login`, {email, password});
   }
 }
