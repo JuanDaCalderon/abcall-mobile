f1e0edb9d00b107515c35798b2dd5df7b9d18f74
Leonardo Barrios Poveda <o.barriosp@uniandes.edu.co>
2024-11-17 21:33:12 -0500

    HU ABC-27 Servicio de chatbot a través de la app móvil

--- src/app/screens/chatbot/chatbot.page.ts
+++ src/app/screens/chatbot/chatbot.page.ts
@@ -1,10 +1,47 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ChatbotService} from '../../services/chatbot.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-chatbot',
   templateUrl: 'chatbot.page.html',
   styleUrls: ['chatbot.page.scss']
 })
-export class ChatbotPage {
-  constructor() {}
+export class ChatbotPage implements OnInit {
+  public hasLoadTranslations: boolean = false;
+  public isLoading: boolean = false;
+  messages: {text: string; sender: 'user' | 'bot'}[] = [];
+  newMessage: string = '';
+
+  constructor(
+    private chatbotService: ChatbotService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.chatbotService.sendMessage('0').subscribe((response) => {
+      const botMessage: {text: string; sender: 'user' | 'bot'} = {text: response.respuesta, sender: 'bot'};
+      this.messages.push(botMessage);
+    });
+  }
+
+  sendMessage() {
+    if (this.newMessage.trim().length === 0) {
+      return;
+    }
+
+    const userMessage: {text: string; sender: 'user' | 'bot'} = {text: this.newMessage, sender: 'user'};
+    this.messages.push(userMessage);
+
+    this.chatbotService.sendMessage(this.newMessage).subscribe((response) => {
+      const botMessage: {text: string; sender: 'user' | 'bot'} = {text: response.respuesta, sender: 'bot'};
+      this.messages.push(botMessage);
+    });
+
+    this.newMessage = '';
+  }
+
+  public home() {
+    this.router.navigate(['/home/consultar_incidentes']);
+  }
 }
